3dbb89644e5ded6d75f0758b79b54997b763e42b
Michel Werren <michel.werren@husqvarnagroup.com>
2023-05-03 05:52:05 +0200

    feat: Initial setup and basic project structure

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    kotlin("multiplatform") version "1.8.21" apply false
+}
+
+subprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
---
+++ creator/build.gradle.kts
@@ -0,0 +1,130 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
+plugins {
+    kotlin("multiplatform")
+}
+
+val kotlinWrapperVersion:String by project
+
+val vertxVersion:String by project
+val jacksonVersion:String by project
+val kotlinCoroutinesVersion:String by project
+val nettyIoUringVersion:String by project
+val kotlinLogginVersion:String by project
+val log4j2Version:String by project
+
+val postgresJdbcVersion:String by project
+val scramClientVersion:String by project
+val liquibaseVersion:String by project
+
+
+val kotestVersion:String by project
+val testcontainersVersion:String by project
+val postgresVersion:String by project
+
+
+fun vertx(module: String): String = "io.vertx:vertx-$module"
+
+kotlin {
+    js(IR) {
+        browser {
+            testTask {
+                testLogging.showStandardStreams = true
+                useKarma {
+                    useChromeHeadless()
+                    useFirefox()
+                }
+            }
+        }
+        binaries.executable()
+    }
+    jvm {
+        withJava()
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+            }
+        }
+        val jsMain by getting {
+            dependencies {
+                implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$kotlinWrapperVersion"))
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react-router-dom")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-mui")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-mui-icons")
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(enforcedPlatform("io.vertx:vertx-dependencies:$vertxVersion"))
+                implementation(enforcedPlatform("com.fasterxml.jackson:jackson-bom:$jacksonVersion"))
+                implementation(enforcedPlatform("org.apache.logging.log4j:log4j-bom:$log4j2Version"))
+                implementation(vertx("core"))
+                implementation(vertx("web"))
+                implementation(vertx("lang-kotlin"))
+                implementation(vertx("lang-kotlin-coroutines"))
+                implementation(vertx("health-check"))
+                implementation(vertx("config"))
+                implementation(vertx("config-yaml"))
+                implementation(vertx("io_uring-incubator"))
+                runtimeOnly("io.netty.incubator:netty-incubator-transport-native-io_uring:$nettyIoUringVersion:linux-x86_64")
+
+                implementation(vertx("pg-client"))
+                implementation(vertx("sql-client-templates"))
+                implementation("org.postgresql:postgresql:$postgresJdbcVersion")
+                implementation("com.ongres.scram:client:$scramClientVersion")
+                implementation("org.liquibase:liquibase-core:$liquibaseVersion")
+
+                implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
+                implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+                implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+                implementation("com.fasterxml.jackson.module:jackson-module-blackbird")
+
+                implementation("org.apache.logging.log4j:log4j-api")
+                implementation("org.apache.logging.log4j:log4j-core")
+                implementation("org.apache.logging.log4j:log4j-slf4j-impl")
+                implementation("io.github.microutils:kotlin-logging:$kotlinLogginVersion")
+
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$kotlinCoroutinesVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:$kotlinCoroutinesVersion")
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(platform("org.testcontainers:testcontainers-bom:$testcontainersVersion"))
+
+                implementation(vertx("junit5"))
+                implementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
+                implementation("org.testcontainers:postgresql")
+            }
+        }
+    }
+}
+
+tasks {
+    getByName("jvmTest", Test::class) {
+        useJUnitPlatform()
+
+        testLogging.exceptionFormat = TestExceptionFormat.FULL
+        testLogging.showCauses = true
+        testLogging.showExceptions = true
+        testLogging.showStackTraces = true
+
+        environment(
+            "vertx.logger-delegate-factory-class-name" to "io.vertx.core.logging.SLF4JLogDelegateFactory",
+            "POSTGRES_VERSION" to postgresVersion,
+        )
+    }
+}
