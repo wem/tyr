000ec4d28d4b633ea4f8b14fa87dfb1c04e5e5d8
Michel Werren <michel.werren@husqvarnagroup.com>
2023-05-15 16:54:18 +0200

    chore: Html Grid extensions

---
+++ creator/src/jsMain/kotlin/ch/sourcemotion/tyr/creator/ui/ext/GridExt.kt
@@ -0,0 +1,17 @@
+package ch.sourcemotion.tyr.creator.ui.ext
+
+import csstype.PropertiesBuilder
+import web.cssom.AlignItems
+import web.cssom.GridAutoFlow
+import web.cssom.JustifyContent
+
+
+fun PropertiesBuilder.centeredGridElements() {
+    justifyContent = JustifyContent.center
+    alignItems = AlignItems.center
+}
+
+fun PropertiesBuilder.rowFlow() {
+    gridAutoFlow = GridAutoFlow.row
+}
+
