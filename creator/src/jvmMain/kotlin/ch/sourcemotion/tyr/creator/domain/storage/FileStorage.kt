8d684cfa2e9ceff54b5d929176d4183f0aefd2dc
Michel Werren <michel.werren@husqvarnagroup.com>
2023-05-23 19:51:21 +0200

    chore: Simplify file storage.

--- creator/src/jvmMain/kotlin/ch/sourcemotion/tyr/creator/domain/storage/FileStorage.kt
+++ creator/src/jvmMain/kotlin/ch/sourcemotion/tyr/creator/domain/storage/FileStorage.kt
@@ -7,61 +7,43 @@ import io.vertx.core.Vertx
 import io.vertx.core.buffer.Buffer
 import io.vertx.core.file.FileSystem
 import io.vertx.kotlin.coroutines.await
-import kotlinx.coroutines.*
 import mu.KLogging
 import java.util.*
 
-interface FileInfoManager {
-    suspend fun saveFileInfoOf(fileId: UUID, mimeType: MimeType, description: String?)
-    suspend fun deleteFileInfoOf(fileId: UUID)
-}
-
 class FileStorage private constructor(
     private val basePath: String,
-    private val fileSystem: FileSystem,
-    private val fileInfoManager: FileInfoManager
+    private val fileSystem: FileSystem
 ) {
     companion object : KLogging() {
-        fun of(vertx: Vertx, config: FileStorageConfig, fileInfoManager: FileInfoManager) =
-            FileStorage(config.baseFilesystemPath, vertx.fileSystem(), fileInfoManager)
+        fun of(vertx: Vertx, config: FileStorageConfig) = FileStorage(config.baseFilesystemPath, vertx.fileSystem())
 
         private val fileExtensionByMimeType =
             mapOf(MimeType.JPEG to "jpg", MimeType.PNG to "png", MimeType.BMP to "bmp", MimeType.MP3 to "mp3")
     }
 
-    suspend fun saveFile(data: Buffer, mimeType: MimeType, description: String?): UUID {
-        val fileId = UUID.randomUUID()
-        val fileName = fileNameOf(fileId, mimeType)
+    suspend fun saveFile(id: UUID, data: Buffer, mimeType: MimeType) {
+        val fileName = fileNameOf(id, mimeType)
         runCatching {
-            coroutineScope {
-                launch { fileSystem.writeFile("$basePath/$fileName", data).await() }
-                launch { fileInfoManager.saveFileInfoOf(fileId, mimeType, description) }
-            }
+            fileSystem.writeFile("$basePath/$fileName", data).await()
         }.onFailure { failure ->
-            logger.error(failure) { "Failed to save file properly. File will get cleaned up" }
-            deleteFile(fileId, mimeType)
-
             throw FileStorageException("File could not be saved", failure)
         }
-        return fileId
     }
 
-    suspend fun getFileContent(id: UUID, mimeType: MimeType) : Buffer? {
+    suspend fun getFileContent(id: UUID, mimeType: MimeType): Buffer? {
         val fileName = fileNameOf(id, mimeType)
         val filePath = "${basePath}/$fileName"
-        return if (fileSystem.exists(filePath).await()) {
+        return runCatching { if (fileSystem.exists(filePath).await()) {
             fileSystem.readFile("${basePath}/$fileName").await()
-        } else null
+        } else null }.getOrElse {
+            throw FileStorageException("Failed to get file '$id' of type '$mimeType'")
+        }
     }
 
     suspend fun deleteFile(id: UUID, mimeType: MimeType) {
         val fileName = fileNameOf(id, mimeType)
         runCatching {
-            val (filesystemDeferred, fileInfoManagerDeferred) = supervisorScope {
-                async { fileSystem.delete("${basePath}/$fileName").await() } to
-                async { fileInfoManager.deleteFileInfoOf(id) }
-            }
-            awaitAll(filesystemDeferred, fileInfoManagerDeferred)
+            fileSystem.delete("${basePath}/$fileName").await()
         }.onFailure {
             logger.warn { "Failed to delete file '$id' of type '$mimeType'" }
         }
--- creator/src/jvmTest/kotlin/ch/sourcemotion/tyr/creator/domain/storage/FileStorageTest.kt
+++ creator/src/jvmTest/kotlin/ch/sourcemotion/tyr/creator/domain/storage/FileStorageTest.kt
@@ -2,8 +2,8 @@ package ch.sourcemotion.tyr.creator.domain.storage
 
 import ch.sourcemotion.tyr.creator.config.FileStorageConfig
 import ch.sourcemotion.tyr.creator.domain.MimeType
+import ch.sourcemotion.tyr.creator.ext.newUUID
 import ch.sourcemotion.tyr.creator.testing.AbstractVertxTest
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
@@ -13,7 +13,6 @@ import io.vertx.kotlin.coroutines.await
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import java.nio.file.Path
-import java.util.*
 
 class FileStorageTest : AbstractVertxTest() {
 
@@ -23,78 +22,22 @@ class FileStorageTest : AbstractVertxTest() {
 
     @Test
     fun `save and get file - success`(testContext: VertxTestContext, @TempDir tempDir: Path) =
-        testContext.async(1) { checkpoint ->
-            val fileDescription = "file description"
+        testContext.async {
+            val sut = FileStorage.of(vertx, FileStorageConfig(tempDir.toFile().absolutePath))
 
-            val sut = FileStorage.of(vertx, FileStorageConfig(tempDir.toFile().absolutePath), object : FileInfoManager {
-                override suspend fun saveFileInfoOf(fileId: UUID, mimeType: MimeType, description: String?) {
-                    testContext.verify { description.shouldBe(fileDescription) }
-                    checkpoint.flag()
-                }
-
-                override suspend fun deleteFileInfoOf(fileId: UUID) {
-                    testContext.failNow("delete must not get called")
-                }
-            })
-
-            val fileId = sut.saveFile(fileContent, MimeType.PNG, fileDescription)
+            val fileId = newUUID()
+            sut.saveFile(fileId, fileContent, MimeType.PNG)
             sut.getFileContent(fileId, MimeType.PNG).shouldBe(fileContent)
         }
 
     @Test
-    fun `save file - file info manager fails`(testContext: VertxTestContext, @TempDir tempDir: Path) =
-        testContext.async(1) { checkpoint ->
-            val sut = FileStorage.of(vertx, FileStorageConfig(tempDir.toFile().absolutePath), object : FileInfoManager {
-                override suspend fun saveFileInfoOf(fileId: UUID, mimeType: MimeType, description: String?) {
-                    throw Exception("File info manager did fail")
-                }
-
-                override suspend fun deleteFileInfoOf(fileId: UUID) {
-                    checkpoint.flag()
-                }
-            })
-
-            shouldThrow<FileStorageException> { sut.saveFile(fileContent, MimeType.PNG, "description") }
-            verifyFileSystemStorageIsEmpty(tempDir)
-        }
-
-
-    @Test
     fun `delete - success`(testContext: VertxTestContext, @TempDir tempDir: Path) =
-        testContext.async(2) { checkpoint ->
-            var savedFileId: UUID? = null
-            val sut = FileStorage.of(vertx, FileStorageConfig(tempDir.toFile().absolutePath), object : FileInfoManager {
-                override suspend fun saveFileInfoOf(fileId: UUID, mimeType: MimeType, description: String?) {
-                    savedFileId = fileId
-                    checkpoint.flag()
-                }
+        testContext.async {
+            val sut = FileStorage.of(vertx, FileStorageConfig(tempDir.toFile().absolutePath))
 
-                override suspend fun deleteFileInfoOf(fileId: UUID) {
-                    testContext.verify { fileId.shouldBe(savedFileId) }
-                    checkpoint.flag()
-                }
-            })
+            val fileId = newUUID()
+            sut.saveFile(fileId, fileContent, MimeType.JPEG)
 
-            val fileId = sut.saveFile(fileContent, MimeType.JPEG, "description")
-            sut.deleteFile(fileId, MimeType.JPEG)
-            sut.getFileContent(fileId, MimeType.PNG).shouldBeNull()
-            verifyFileSystemStorageIsEmpty(tempDir)
-        }
-
-    @Test
-    fun `delete - file info manager fails`(testContext: VertxTestContext, @TempDir tempDir: Path) =
-        testContext.async(1) { checkpoint ->
-            val sut = FileStorage.of(vertx, FileStorageConfig(tempDir.toFile().absolutePath), object : FileInfoManager {
-                override suspend fun saveFileInfoOf(fileId: UUID, mimeType: MimeType, description: String?) {
-                    checkpoint.flag()
-                }
-
-                override suspend fun deleteFileInfoOf(fileId: UUID) {
-                    throw Exception("File info manager did fail")
-                }
-            })
-
-            val fileId = sut.saveFile(fileContent, MimeType.JPEG, "description")
             sut.deleteFile(fileId, MimeType.JPEG)
             sut.getFileContent(fileId, MimeType.PNG).shouldBeNull()
             verifyFileSystemStorageIsEmpty(tempDir)
