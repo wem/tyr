b4ea14032662c63eb87e44901e51b80d5fa757a1
Michel Werren <michel.werren@husqvarnagroup.com>
2023-05-25 06:49:31 +0200

    chore: Add media http content type to mime type dto

--- creator/src/commonMain/kotlin/ch/sourcemotion/tyr/creator/dto/element/MimeTypeDto.kt
+++ creator/src/commonMain/kotlin/ch/sourcemotion/tyr/creator/dto/element/MimeTypeDto.kt
@@ -3,9 +3,13 @@ package ch.sourcemotion.tyr.creator.dto.element
 import kotlinx.serialization.Serializable
 
 @Serializable
-enum class MimeTypeDto {
+enum class MimeTypeDto(val httpContentType: String) {
     // Image
-    JPEG, PNG, BMP,
+    JPEG("image/jpeg"), PNG("image/png"), BMP("image/bmp"),
     // Sound
-    MP3
+    MP3("audio/mpeg");
+
+    companion object {
+        fun ofContentType(httpContentType: String) = values().first { it.httpContentType == httpContentType }
+    }
 }
